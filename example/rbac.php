500c8b4b5deacf7aff73bb44811ba1624c05b330
andrey <plandem@gmail.com>
2013-10-16 18:52:27 +0400

    +added example

---
+++ example/rbac.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Gayvoronsky Andrey <plandem@gmail.com>
+ * @version 1.0
+ */
+
+echo json_encode(array(
+		"Guest" => false,
+		"Admin" => true,
+		"User" => false,
+		"User.Update" => true,
+	)
+);
